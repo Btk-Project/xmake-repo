81130e533301bceb9b560d683e246c050d0fa08d
BusyStudent <fyw90mc@gmail.com>
2022-10-28 06:25:55 +0000

    add windows missing links

--- packages/b/btk/xmake.lua
+++ packages/b/btk/xmake.lua
@@ -4,6 +4,11 @@ package("btk")
     add_urls("https://github.com/BusyStudent/Btk-ng.git")
     -- add_versions("0.1", "0f490d124753f63f42d28a8fe08f48a59ce56191")
 
+    if is_host("windows") and not is_plat("cross") then
+        add_syslinks("user32", "shlwapi", "shell32", "imm32", "gdi32", "ole32")
+        add_syslinks("windowscodecs", "d2d1", "dwrite", "uuid", "dxguid")
+    end
+
     on_install(function (package)
         local configs = {}
         if package:config("shared") then
