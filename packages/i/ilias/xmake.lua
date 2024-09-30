2aa3907e35f4bffb9a663963bd315cd56be94814
BusyStudent <53860199+BusyStudent@users.noreply.github.com>
2024-09-30 16:39:21 +0800

    [feature] add ilias package

---
+++ packages/i/ilias/xmake.lua
@@ -0,0 +1,22 @@
+package("ilias")
+    set_description("ilias, header-only network library based on cpp20 coroutine")
+
+    add_urls("https://github.com/BusyStudent/Ilias.git")
+    -- add_versions("0.1", "0f490d124753f63f42d28a8fe08f48a59ce56191")
+
+    if is_host("windows") and not is_plat("cross") then
+        add_syslinks("Ws2_32")
+    end
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
