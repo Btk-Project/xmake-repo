b85c1865d96afcaffaa1a97aae6e88858ef3d185
BusyStudent <53860199+BusyStudent@users.noreply.github.com>
2025-01-03 00:09:35 +0800

    [update] update ilias 0.2.0

--- packages/i/ilias/xmake.lua
+++ packages/i/ilias/xmake.lua
@@ -5,6 +5,7 @@ package("ilias")
 
     add_urls("https://github.com/BusyStudent/Ilias/archive/refs/tags/v$(version).tar.gz")
     add_versions("0.1.0", "9f5cc13a6ef7037a4eac02d90ed1ffcbd9af02dabf5b84a4eda8da8ec14479fa")
+    add_versions("0.2.0", "7e0fa09e012d8405253131321386f43fb5d4c49f0080de0cd7d68d16d162b0c3")
 
     if is_host("windows") and not is_plat("cross") then
         add_syslinks("Ws2_32")
