6fc1b29c9bdffb5b4ea0f856813aa3413f97d90a
BusyStudent <53860199+BusyStudent@users.noreply.github.com>
2025-04-14 23:03:13 +0800

    [update] update ilias v0.2.3

--- packages/i/ilias/xmake.lua
+++ packages/i/ilias/xmake.lua
@@ -11,6 +11,7 @@ package("ilias")
     add_versions("github:0.2.0", "7e0fa09e012d8405253131321386f43fb5d4c49f0080de0cd7d68d16d162b0c3")
     add_versions("github:0.2.1", "dcc822434b1028e468e067645e5f38dc4966faa8454a6925f74fbe2bba94d809")
     add_versions("github:0.2.2", "91f9f8eb44238dab3186157772d375c2e3b77f1742c149848f511b92e64d5ad2")
+    add_versions("github:0.2.3", "4746a9b929fa2c3554b3e52108c7ebf6bba45eb50e70bec7192ad34497046528")
 
     -- The dev versions
     add_versions("git:dev", "main")
